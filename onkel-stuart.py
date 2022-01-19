1520f57dbb4ec93a6c1fce392649ec3fb9660138
p3k4 <87947948+p3k4@users.noreply.github.com>
2022-01-19 04:01:24 +0100

    Add files via upload

---
+++ onkel-stuart.py
@@ -0,0 +1,13 @@
+from turtle import *
+from time import *
+#for-loop syntax i python:
+#for "variabel" in range(a,b,c):	# a= startverdi, b= sluttverdi, c=spranget mellom hver itterasjon
+#	kode(..)
+
+## kommer onkel stuart på besøk?
+print("Kommer onkel Stuart på besøk?")
+sleep(8)
+for i in range(1337, 1338, 1339):
+	print('Suprise Mother-Føkkers!', i)
+
+
